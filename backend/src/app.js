5cff05d87a216b5b408e4c213f8499f4cf306f32
Utkarsh Singh <94187449+HSRAKTU@users.noreply.github.com>
2025-01-06 20:45:08 +0530

    add extended classes for API error and response

---
+++ backend/src/app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+// import cors from 'cors';
+// import cookieParser from 'cookie-parser';
+
+const app = express();
+
+// app.use(cors({
+//     //lorigin: ['https://tm-reunion.vercel.app','https://www.tm-reunion.vercel.app'],
+//     credentials: true, 
+// }));
+
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
+// app.use(express.static("public"));
+// app.use(cookieParser());
+
+
+//routes
+
+
+//routes declaration
+
+export { app };
