a976883acb8009d87518bdf5deb1ee35329361a3
Utkarsh Singh <94187449+HSRAKTU@users.noreply.github.com>
2025-01-06 21:45:17 +0530

    add user model

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,18 @@
 import express from 'express';
-// import cors from 'cors';
-// import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
-// app.use(cors({
-//     //lorigin: ['https://tm-reunion.vercel.app','https://www.tm-reunion.vercel.app'],
-//     credentials: true, 
-// }));
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true, 
+}));
 
-// app.use(express.json());
-// app.use(express.urlencoded({extended: true}));
-// app.use(express.static("public"));
-// app.use(cookieParser());
+app.use(express.json({limit: "16Kb"}));
+app.use(express.urlencoded({extended: true, limit: "16kb"}));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 
 //routes
